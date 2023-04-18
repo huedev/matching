d62f4233c3ae09fbae2d7d9d377f796539523181
hue <32443158+huedev@users.noreply.github.com>
2023-04-18 00:41:59 -0500

    Project setup

---
+++ components/header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="my-4 flex items-center justify-between">
+      <h1 className="font-semibold text-xl text-gray-900 dark:text-gray-100">Matching</h1>
+    </header>
+  )
+}
