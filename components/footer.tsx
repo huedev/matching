d2bdb6887c1dcb11601e1c6f0ef25a5f0f5e7e8b
hue <32443158+huedev@users.noreply.github.com>
2023-04-20 11:32:07 -0500

    Repo link in footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,9 +1,10 @@
 export default function Footer() {
   return (
-    <footer className="mt-12 mb-8 flex items-center justify-center">
-      <span className="font-semibolds text-gray-600 dark:text-gray-400">
+    <footer className="mt-12 mb-8 flex items-center justify-between">
+      <span className="text-gray-600 dark:text-gray-400">
         A game by <a className="text-gray-950 dark:text-white" href="https://www.huedev.me/" rel="noopener" target="_blank">huedev</a>
       </span>
+      <a className="text-gray-950 dark:text-white" href="https://github.com/huedev/matching" rel="noopener" target="_blank">View on GitHub</a>
     </footer>
   )
 }
